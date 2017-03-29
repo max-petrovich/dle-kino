6631c6b2677bd00a65a052e8b493a3de4679b947
Maxim <unrelaxby@gmail.com>
2017-03-29 20:12:32 +0300

    Start

---
+++ engine/data/dbconfig.php
@@ -0,0 +1,21 @@
+<?PHP
+
+define ("DBHOST", "localhost"); 
+
+define ("DBNAME", "dle103u");
+
+define ("DBUSER", "root");
+
+define ("DBPASS", "");  
+
+define ("PREFIX", "dle"); 
+
+define ("USERPREFIX", "dle");
+
+define ("COLLATE", "utf8"); 
+
+define('SECURE_AUTH_KEY', '3(j31vN+Ehxf|OPN C?jF0e/(i}jJLjId~P|8f`],Pk=*tCGD|+zm4#<zgi!npa{');
+
+$db = new db;
+ 
+?>
